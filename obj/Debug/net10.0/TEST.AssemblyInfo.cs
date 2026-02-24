2ee9cfa4f548f5f2a1d83a0e64841eafe5855674
TenCuaBan <email@example.com>
2026-02-24 13:35:48 +0700

    Cap nhat moi

--- obj/Debug/net10.0/TEST.AssemblyInfo.cs
+++ obj/Debug/net10.0/TEST.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TEST")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+582fd74831e7c998296214f347aeba67985ca822")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33e900dfd69d543ab33450a5dfebb066cbaddaa9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TEST")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TEST")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
